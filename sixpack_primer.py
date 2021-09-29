f3a89539e42dc7b15f70c8495efefdebd377bad1
cjpark989 <86919286+cjpark989@users.noreply.github.com>
2021-09-28 22:52:34 -0400

    Delete sixpack_primer.py

--- sixpack_primer.py
+++
@@ -1,9 +0,0 @@
-# some_file.py
-import sys
-# insert at 1, 0 is the script path (or '' in REPL)
-sys.path.insert(1, '/Users/chungjunepark/Documents/Lab')
-
-from methods import *
-
-#rnd-1_family-33, unknown, and length greater than 1000: 1000.bed
-sixpack_primer("unknown_fam_33")
